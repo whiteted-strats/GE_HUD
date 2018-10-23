58617aabf4027bb9cb403a25340ee37b7b3b1601
whiteted-strats <whiteted.strats@gmail.com>
2018-10-23 22:34:01 +0100

    Addition to Wyster's library, for HUD_last_shot

---
+++ GuardData_appendage.lua
@@ -0,0 +1,16 @@
+GuardData.metadata =
+{
+    ...
+
+    
+    -- ============== Gunshot mayhem! =================
+    
+    -- 0xFF most of the time, 0x01,0x02, maybe other numbers, during the shooting.
+    --   Maybe angle is fixed at 01, fired 02, FF idle?
+    {["offset"] = 0x180, ["size"] = 0x1, ["type"] = "hex", ["name"] = "shooting_stage_flag"},
+	-- Definitely is the end of the gun barrel: confirmed on HUD
+	{["offset"] = 0x184, ["size"] = 0xC, ["type"] = "vector",	["name"] = "shot_origin"},
+	-- Is very likely the unit direction of the effective bullet leaving the guard's gun
+    {["offset"] = 0x190, ["size"] = 0xC, ["type"] = "vector",	["name"] = "bullet_dirc"}
+    
+}
